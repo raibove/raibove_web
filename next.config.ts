afe0f045dd6b33790172225114acf7e775a7a78d
raibove <shwetakale144@gmail.com>
2025-08-15 10:15:08 +0530

    trying to add cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import createMDX from '@next/mdx'
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 }
 
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+ 
 const withMDX = createMDX({
   options: {
     remarkPlugins: [],
